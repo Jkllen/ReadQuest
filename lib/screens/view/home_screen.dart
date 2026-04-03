c7ed893bd1418fb78ad1a563faafbdf46b524745
Dexonsayder <martingreggcorruz@gmail.com>
2026-04-03 21:06:42 +0800

    transparency updated on dashboard to show background

--- lib/screens/view/home_screen.dart
+++ lib/screens/view/home_screen.dart
@@ -49,7 +49,28 @@ class HomeScreenState extends State<HomeScreen> {
     ];
 
     return Scaffold(
-      body: pages[currentIndex],
+      body: Stack(
+        children: [
+          AnimateGradient(
+            primaryBegin: Alignment.topLeft,
+            primaryEnd: Alignment.bottomLeft,
+            secondaryBegin: Alignment.bottomLeft,
+            secondaryEnd: Alignment.topRight,
+            primaryColors: const [
+              Color.fromARGB(255, 200, 112, 255),
+              Color.fromARGB(255, 219, 169, 250),
+              Colors.white,
+            ],
+            secondaryColors: const [
+              Colors.white,
+              Color.fromARGB(255, 170, 201, 255),
+              Color.fromARGB(255, 108, 208, 255),
+            ],
+            duration: const Duration(seconds: 30),
+          ),
+          pages[currentIndex],
+        ],
+      ),
       bottomNavigationBar: NavigationBar(
         selectedIndex: currentIndex,
         onDestinationSelected: onTap,
