89d30501e573e81a87d6641ba23cf7cab56d5f6d
ywssp <46750010+ywssp@users.noreply.github.com>
2026-04-14 22:03:52 +0800

    Recolor Stats screen

--- lib/styles/app_colors.dart
+++ lib/styles/app_colors.dart
@@ -14,7 +14,7 @@ class AppPalette {
   static const yellow = Color(0xFFffc857);
 }
 
-class AppColors extends AppPalette {
+class AppColors {
   static const homeBackground = Color(0xFFF9FAFB);
   static const navigationBar = AppPalette.black;
   static const accent = Color(0xFF2078FC);
@@ -22,7 +22,6 @@ class AppColors extends AppPalette {
   static const card = _CardColors();
 
   static const home = _HomeColors();
-  static const read = _ReadColors();
   static const difficulty = _DifficultyColors();
   static const rewards = _RewardsColors();
   static const stats = _StatsColors();
@@ -30,7 +29,7 @@ class AppColors extends AppPalette {
   static const boss = _BossColors();
 }
 
-class _CardColors extends AppPalette {
+class _CardColors {
   const _CardColors();
 
   final text = Colors.white;
@@ -41,7 +40,7 @@ class _CardColors extends AppPalette {
   final iconBackground = Colors.white24;
 }
 
-class _HomeColors extends AppPalette {
+class _HomeColors {
   const _HomeColors();
 
   final xpCard = AppPalette.blue;
@@ -51,14 +50,7 @@ class _HomeColors extends AppPalette {
   final wordsCard = AppPalette.aqua;
 }
 
-class _ReadColors extends AppPalette {
-  const _ReadColors();
-  final easy = const Color(0xFF05DF72);
-  final medium = const Color(0xFFFDC700);
-  final hard = const Color(0xFFDF0505);
-}
-
-class _DifficultyColors extends AppPalette {
+class _DifficultyColors {
   const _DifficultyColors();
 
   final easy = const Color(0xFF05DF72);
@@ -66,31 +58,31 @@ class _DifficultyColors extends AppPalette {
   final hard = const Color(0xFFDF0505);
 }
 
-class _RewardsColors extends AppPalette {
+class _RewardsColors {
   const _RewardsColors();
 
   // TODO: Move rewards colors here
 }
 
-class _StatsColors extends AppPalette {
+class _StatsColors {
   const _StatsColors();
 
-  final positiveGrowth = const Color(0xFF22C55E);
-  final negativeGrowth = const Color(0xFFEF4444);
+  final positiveGrowth = const Color(0xFF10B981);
+  final negativeGrowth = const Color(0xFFB9103A);
 
   final comprehensionCard = const Color(0xFF3B82F6);
   final vocabularyCard = const Color(0xFFA855F7);
-  final speedCard = const Color(0xFF10B981);
+  final speedCard = const Color(0xFFB9A210);
 }
 
-class _QuizColors extends AppPalette {
+class _QuizColors {
   const _QuizColors();
 
   final correct = const Color(0xFF22C55E);
   final incorrect = const Color(0xFFEF4444);
 }
 
-class _BossColors extends AppPalette {
+class _BossColors {
   const _BossColors();
 
   final bgStartColor = const Color(0xFF160A34);
